9b9c2ff6d422623eb402a03337a39ba1c93d9f8e
jayesh200026 <jayeshkumar8660@gmail.com>
2021-09-09 06:42:57 +0530

    Formatted code to look better

--- InventoryJson/src/com/json/InventoryManagement.java
+++ InventoryJson/src/com/json/InventoryManagement.java
@@ -17,64 +17,66 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class InventoryManagement {
-	static Map<String,Double> map = new HashMap<>();
+	static Map<String, Double> map = new HashMap<>();
+
 	public static void inventoryManager() {
 		List<JSONObject> list = inventoryFactory();
-		
-		for(JSONObject obj : list) {
-			String type=(String) obj.get("type");
-			String name=(String) obj.get("name");
-			double weight=(double) obj.get("weight");
+
+		for (JSONObject obj : list) {
+			String type = (String) obj.get("type");
+			String name = (String) obj.get("name");
+			double weight = (double) obj.get("weight");
 			double pricePerWeight = (double) obj.get("pricePerWeight");
-			double totalPrice = pricePerWeight*weight;
+			double totalPrice = pricePerWeight * weight;
 			map.put(name, totalPrice);
 		}
-		
+
 		displayJSON();
-		
+
 	}
+
 	private static void displayJSON() {
-		JSONArray array =  new JSONArray();
-		for (String s: map.keySet()) {
-			//object1 = new JSONObject();
-			JSONObject object1=new JSONObject();
+		JSONArray array = new JSONArray();
+		for (String s : map.keySet()) {
+			// object1 = new JSONObject();
+			JSONObject object1 = new JSONObject();
 			object1.put("name", s);
-			object1.put("Totalprice",map.get(s));
+			object1.put("Totalprice", map.get(s));
 			array.add(object1);
 		}
-		//System.out.println(array);
-		JSONObject mainObject =  new JSONObject();
+		// System.out.println(array);
+		JSONObject mainObject = new JSONObject();
 		mainObject.put("Result", array);
 		System.out.println(mainObject.toJSONString());
-		
+
 	}
+
 	private static List<JSONObject> inventoryFactory() {
-	
+
 		List<JSONObject> list = new ArrayList<JSONObject>();
 		JSONParser parser = new JSONParser();
 		try {
-			Reader reader = new FileReader("/Users/jayeshkumar/learning_path/BATCH51/oop/InventoryJson/data/inventory.json");
+			Reader reader = new FileReader(
+					"/Users/jayeshkumar/learning_path/BATCH51/oop/InventoryJson/data/inventory.json");
 			JSONObject inventory = (JSONObject) parser.parse(reader);
-			JSONArray array= (JSONArray) inventory.get("inventory");
+			JSONArray array = (JSONArray) inventory.get("inventory");
 			Iterator<JSONObject> iterator = array.iterator();
-			while(iterator.hasNext()) {
-				JSONObject object2=iterator.next();
+			while (iterator.hasNext()) {
+				JSONObject object2 = iterator.next();
 				list.add(object2);
 			}
-			
-			
-		}
-		catch (FileNotFoundException e) {
+
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-		
-		
+
 		return list;
 	}
+
 	public static void main(String args[]) {
 		InventoryManagement.inventoryManager();
 	}
